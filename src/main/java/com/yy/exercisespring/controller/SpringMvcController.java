690dc9df7cc5ab423e899bbc50982eb82082e210
xide <xdz12530@ly.com>
2020-08-28 23:04:39 +0800

    requestMapping相关

--- src/main/java/com/yy/exercisespring/controller/SpringMvcController.java
+++ src/main/java/com/yy/exercisespring/controller/SpringMvcController.java
@@ -1,14 +1,51 @@
 package com.yy.exercisespring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("spring/mvc")//映射
 public class SpringMvcController {
 
     @RequestMapping("welcome")
-    public String welcome()
-    {
-        return "success";
+    public String welcome() {
+        return "success";  // 默认使用了请求转发的跳转方式
     }
+
+    @RequestMapping(value = "welcome1", method = RequestMethod.POST, params = {"name=xdz", "age!=23", "!height"})
+    public String welcome1() {
+        return "success";  // 默认使用了请求转发的跳转方式
+    }
+
+    @RequestMapping(value = "welcome2", method = RequestMethod.POST, headers = {"Accept-Encoding=gzip, deflate", ""})
+    public String welcome2() {
+        return "success";  // 默认使用了请求转发的跳转方式
+    }
+
+
+    /*
+    ant风格的请求路径
+    ?单字符
+    *任意个字符(0个或多个)
+    **任意目录
+     */
+
+    @RequestMapping(value = "welcome3/**/test")
+    public String welcome3() {
+        return "success";  // 默认使用了请求转发的跳转方式
+    }
+
+    @RequestMapping(value = "welcome4/a?c/test")
+    public String welcome4() {
+        return "success";  // 默认使用了请求转发的跳转方式
+    }
+
+    @RequestMapping(value = "welcome5/{name}")
+    public String welcome5(@PathVariable("name") String name) {
+        System.out.println(name);
+        return "success";  // 默认使用了请求转发的跳转方式
+    }
+
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,6 +1,27 @@
 <html>
 <body>
 <h2>Hello World!</h2>
-<a href="/welcome"> first mvc </a>
+<a href="spring/mvc/welcome"> first mvc </a> <br>
+
+<a href="spring/mvc/welcome1"> first mvc welcome1</a> <br>
+
+<form action="/spring/mvc/welcome1" method="post">
+    welcome1<br>
+    name:<input name="name">  <br>
+    age:<input name="age">  <br>
+    <input type="submit" value="post">  <br>
+</form>
+
+<form action="/spring/mvc/welcome2" method="post">
+    welcome2<br>
+    name:<input name="name">  <br>
+    age:<input name="age">  <br>
+    <input type="submit" value="post">  <br>
+</form>
+
+<a href="spring/mvc/welcome3/asdas/test"> first mvc welcome3</a> <br>
+<a href="spring/mvc/welcome4/abc/test"> first mvc welcome4</a> <br>
+<a href="spring/mvc/welcome5/xdz"> first mvc welcome5</a> <br>
+
 </body>
 </html>
