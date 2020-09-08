19e9f6498ece9a5ec032e97253cfedd43951ff0e
xide <xdz12530@ly.com>
2020-09-08 23:25:38 +0800

    12--拦截器

---
+++ src/main/java/com/yy/exercisespring/common/interceptor/MySecondInterceptor.java
@@ -0,0 +1,26 @@
+package com.yy.exercisespring.common.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MySecondInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        System.out.println("拦截请求2222");
+        return true;// true,拦截操作后仍然放行;false:拦截之后不放行,请求终止
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+        System.out.println("拦截响应2222");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+        System.out.println("视图jsp渲染完毕2222");
+    }
+}
